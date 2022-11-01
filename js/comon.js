666874e1002742ce4153718ce43d99c36785f590
SubinNoh <snoh6839@mysvc.skagit.edu>
2022-11-01 11:32:49 +0900

    ddd

--- js/comon.js
+++ js/comon.js
@@ -1,3 +1,25 @@
+window.addEventListener("wheel", function(e){
+  e.preventDefault();},{passive : false});
+  var mHtml = $("html");
+  var page = 1;
+  mHtml.animate({scrollTop : 0},10);
+
+  $(window).on("wheel", function(e) {
+      if(mHtml.is(":animated")) return;
+      if(e.originalEvent.deltaY > 0) {
+          if(page == 9) return;
+          page++;
+      } else if(e.originalEvent.deltaY < 0) {
+          if(page == 1) return;
+          page--;
+      }
+      var posTop = (page-1) * $(window).height();
+      mHtml.animate({scrollTop : posTop});
+
+      var cur_pos = $(this).scrollTop();
+
+  })
+
 $(function() {
   $(window).scroll(function() {
     var winHeight = $(window).height();
