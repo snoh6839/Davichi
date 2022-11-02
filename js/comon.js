db3f894f5ca101a108a1fe37976a575f5655a0e8
SubinNoh <snoh6839@mysvc.skagit.edu>
2022-11-02 11:01:34 +0900

    count

--- js/comon.js
+++ js/comon.js
@@ -35,37 +35,32 @@ $(function() {
   }
 });
 
+const fambtn = document.querySelector('.familysite-btn');
+
+fambtn.addEventListener('click', () => {
+  fambtn.classList.toggle('clicked');
+});
 
 });
 
 const counter = ($counter, max) => {
   let now = max;
-
   const handle = setInterval(() => {
     $counter.innerHTML = Math.ceil(max - now);
-
     // 목표수치에 도달하면 정지
     if (now < 1) {
       clearInterval(handle);
     }
-
     // 증가되는 값이 계속하여 작아짐
     const step = now / 10;
-
     // 값을 적용시키면서 다음 차례에 영향을 끼침
     now -= step;
   }, 50);
 }
 
 window.onload = () => {
-  setTimeout(() => counter( document.querySelector(".count1"), 283), 2000);
-  setTimeout(() => counter( document.querySelector(".count2"), 1418), 2000);
-  setTimeout(() => counter( document.querySelector(".count3"), 41408), 2000);
-  setTimeout(() => counter( document.querySelector(".count4"), 1200), 2000);
-
-  const fambtn = document.querySelector('.familysite-btn');
-
-  fambtn.addEventListener('click', () => {
-    fambtn.classList.toggle('clicked');
-  })
+  setTimeout(() => counter( document.querySelector(".count1"), 283), 3000);
+  setTimeout(() => counter( document.querySelector(".count2"), 418), 3000);
+  setTimeout(() => counter( document.querySelector(".count3"), 408), 3000);
+  setTimeout(() => counter( document.querySelector(".count4"), 200), 3000);
 }
